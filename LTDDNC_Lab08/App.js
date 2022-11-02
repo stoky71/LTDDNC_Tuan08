01c96cc65c865a6db46dd826f70cb8dde25dcfc0
stoky71 <81395239+stoky71@users.noreply.github.com>
2022-11-02 21:03:01 +0700

    finish bai2

--- LTDDNC_Lab08/App.js
+++ LTDDNC_Lab08/App.js
@@ -1,10 +1,10 @@
 import { useState } from 'react';
-import { Animated, StyleSheet, Text, View } from 'react-native';
+import { Animated, Dimensions, StyleSheet, Text, View } from 'react-native';
 
 import jerry from './assets/jerry.png'
 import oggy from './assets/oggy.png'
 import stop from './assets/stop.gif'
-export default function App() {
+function Bai1() {
 
   const [location, setLocation] = useState({
     x: null,
@@ -84,9 +84,107 @@ export default function App() {
   );
 }
 
+function bai2(){
+  const [location1, setLocation1] = useState({
+    x: null,
+    y: null,
+    marginLeft1: new Animated.Value(10),
+    marginTop1: new Animated.Value(10)
+  })
+  
+  const {marginLeft1,marginTop1} = location1
+  
+
+  const [location2, setLocation2] = useState({
+    x: null,
+    y: null,
+    marginLeft2: new Animated.Value(10),
+    marginTop2: new Animated.Value(10)
+  })
+  
+  const {marginLeft2,marginTop2} = location2
+  
+
+  const [location3, setLocation3] = useState({
+    x: null,
+    y: null,
+    marginLeft3: new Animated.Value(10),
+    marginTop3: new Animated.Value(10)
+  })
+  
+  const {marginLeft3,marginTop3} = location3
+
+  function randomLeft(){
+    return Math.min(Math.random() * 300, Dimensions.get('window').width)
+  } 
+  function randomTop(){
+    return Math.min(Math.random() * 500, Dimensions.get('window').height)
+  }
+  function onPress(event){
+    setLocation1({
+      marginLeft1: randomLeft(),
+      marginTop1: randomTop()
+    })
+
+    setLocation2({
+      marginLeft2: randomLeft(),
+      marginTop2: randomTop()
+    })
+
+    setLocation3({
+      marginLeft3: randomLeft(),
+      marginTop3: randomTop()
+    })
+  }
+  function onMove(event){
+    console.log('onMove')
+    console.log('location')
+  }
+  function onRelease(){
+    setLocation1({
+      marginLeft1: randomLeft(),
+      marginTop1: randomTop()
+    })
+
+    setLocation2({
+      marginLeft2: randomLeft(),
+      marginTop2: randomTop()
+    })
+
+    setLocation3({
+      marginLeft3: randomLeft(),
+      marginTop3: randomTop()
+    })
+  }  
+  return (
+    <View style={styles.container}
+      onStartShouldSetResponder={() => true}
+      onMoveShouldSetResponder={() => true}
+      onResponderGrant={onPress}
+      onResponderMove={onMove}
+      onResponderRelease={onRelease}
+      >
+      <Animated.Image
+          source={jerry}
+          style={{transition: '0.5s', marginLeft:marginLeft1, marginTop:marginTop1, height:100, width:100}}>
+      </Animated.Image>
+      <Animated.Image
+          source={jerry}
+          style={{transition: '0.5s', marginLeft:marginLeft2, marginTop:marginTop2, height:100, width:100}}>
+      </Animated.Image>
+      <Animated.Image
+          source={jerry}
+          style={{transition: '0.5s', marginLeft:marginLeft3, marginTop:marginTop3, height:100, width:100}}>
+      </Animated.Image>
+    </View>
+  );
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
   },
 });
+
+export default bai2
