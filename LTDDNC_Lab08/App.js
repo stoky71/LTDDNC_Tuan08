2103d2ca0e355d48834a4b17100464ce7408f5fe
stoky71 <81395239+stoky71@users.noreply.github.com>
2022-11-02 14:15:40 +0700

    finish cau 1

---
+++ LTDDNC_Lab08/App.js
@@ -0,0 +1,70 @@
+import { useState } from 'react';
+import { Animated, StyleSheet, Text, View } from 'react-native';
+
+import jerry from './assets/jerry.png'
+import oggy from './assets/oggy.png'
+import stop from './assets/stop.gif'
+export default function App() {
+
+  const [location, setLocation] = useState({
+    x: null,
+    y: null,
+    marginLeft: new Animated.Value(1),
+    marginTop: new Animated.Value(1)
+  })
+
+  function onPress(event){
+    console.log('=====run=====')
+    var x = event.nativeEvent.locationX
+    console.log('=====run=====')
+    console.log(x)
+    console.log('=====run=====')
+    var y = event.nativeEvent.locationY  
+    console.log('=====run=====')
+    setLocation({
+      x:x,
+      y:y,
+      marginLeft:x,
+      marginTop:y
+    })
+  }
+  function onMove(event){
+    console.log('=====run=====')
+    console.log(location)
+    console.log('=====run=====')
+  }
+  function onRelease(){
+    console.log('=====release=====')
+    
+  }
+  const {marginLeft,marginTop} = location
+  return (
+    <View style={styles.container}
+      onStartShouldSetResponder={() => true}
+      onMoveShouldSetResponder={() => true}
+      onResponderGrant={onPress}
+      onResponderMove={onMove}
+      onResponderRelease={onRelease}>
+      {/* <Text>....</Text> */}
+      {/* <View style={{flex:0.2, alignContent:'center', justifyContent:'center'}}>
+        <Animated.Image
+          source={jerry}
+          style={{marginLeft:marginLeft, marginTop:marginTop, height:100, width:100}}>
+        </Animated.Image>
+      </View> */}
+      <Animated.Image
+          source={stop}
+          style={{marginLeft:marginLeft, marginTop:marginTop, height:150, width:150}}>
+      </Animated.Image>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
